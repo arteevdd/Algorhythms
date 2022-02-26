25aa385250418e466fa4853488b176d2ecc3faf2
Arteev Danil <arteic4@yandex.ru>
2022-02-26 23:30:15 +0300

    BinarySearchTree

---
+++ BinarySearchTree/BinaryTreeTask/BinaryTreeTask/BinaryTreeTask.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include "BinaryTreeTask.h"
+
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	
+	BinarySearchTree<int> tree2; 
+	tree2.insert(20);
+	tree2.insert(15);
+	tree2.insert(60);
+	tree2.insert(70);
+	tree2.insert(30);
+	tree2.insert(45);	
+	tree2.insert(21);
+
+	std::cout << "Первое дерево:\n";
+
+	tree2.print(std::cout);
+
+	/*tree2.deleteKey(60);
+	tree2.insert(13);*/
+	std::cout << "------------------------------------------------------------------"<<std::endl;
+
+	std::cout << "Второе дерево:\n";
+	
+	/*std::cout<<"Узел со значением 11 не был найден: " << tree2.iterativeSearch(11) << std::endl;	
+	std::cout << "Узел со значением 21 был найден: "  << tree2.iterativeSearch(21) << std::endl;*/
+	std::cout << "Высота дерева: " << tree2.getHeight() << std::endl;
+
+
+}
+
